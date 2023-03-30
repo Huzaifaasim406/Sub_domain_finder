102ce4288c700ef107bc5e9ce168d1fd5d2e5148
Huzaifaasim406 <100348114+Huzaifaasim406@users.noreply.github.com>
2023-03-30 05:30:20 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -9,7 +9,12 @@ if [ ! -d "$url/recon" ];then
 mkdir $url/recon
 fi
 
-echo "finding subdomains..."
+echo "finding subdomains from assetfinder...."
 assetfinder $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >>$url/recon/final.txt
+cat $url/recon/assets.txt | grep $1 >>$url/recon/asset_finder.txt
 rm $url/recon/assets.txt
+
+echo "Finding subdomains from amass..."
+amass enum -d $url >> $url/recon/f1..txt
+sort -u $url/recon/f1.txt >> $url/recon/amass_finder.txt
+rm $url/recon/f1.txt
